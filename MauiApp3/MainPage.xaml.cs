fb7d6ce267fd09d13b2f9c1a8c555dfb59d7c34f
Emers <Emers@KIRITO-PC>
2023-07-09 15:41:32 -0600

    Agregar archivos de proyecto.

---
+++ MauiApp3/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace MauiApp3;
+
+public partial class MainPage : ContentPage
+{
+
+	private TimeOnly time = new();
+	private bool isRunning;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+	private async void OnStarStop(object sender, EventArgs e)
+	{
+		isRunning = !isRunning;
+		startsStopButton.Source = isRunning ? "pause" : "play";
+
+		while(isRunning)
+		{
+			time = time.Add(TimeSpan.FromSeconds(1));
+			setTime();
+			await Task.Delay(TimeSpan.FromSeconds(1));
+		}
+	}
+
+	private void OnReset(object sender, EventArgs e)
+	{
+		time = new TimeOnly();
+		setTime();
+	}
+
+
+
+    private void setTime()
+	{
+		Timer.Text = $"{time.Minute}:{time.Second:000}";
+	}
+	
+}
+
